09ea78c758f4ab1fba27b9e19c735aa3b3430f12
Moti-Sultan <motisultannur@gmail.com>
2023-03-16 14:06:19 +0100

    Exercice PROPS with variable const

---
+++ src/ClassComponents.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Intro extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // return <h2>I am a {this.props.model}!</h2>;
+    return <h2> Hi name is {this.props.nom}, I live in {this.props.ville} </h2>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Intro nom="Sultan2" ville="Château Thierry" />);
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class Intro extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    // return <h2>I am a {this.props.model}!</h2>;
-    return <h2> Hi name is {this.props.nom}, I live in {this.props.ville} </h2>
-  }
+function Person(props) {
+  return <h2>I'm {props.nom}, I am a {props.developer} by profession!</h2>;
+}
+
+function Intro() {
+  const nom = "Sultan";
+  const profession = "Web Developer Full stack";
+  return (
+    <>
+      <Person nom={nom} developer={profession} />
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Intro nom="Sultan2" ville="Château ThierrY" />);
+root.render(<Intro />);
 
 
