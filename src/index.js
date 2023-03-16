e334d80d14dabf9090c3f531edf9f612440646d8
Moti-Sultan <motisultannur@gmail.com>
2023-03-16 13:30:56 +0100

    First Commit Learning REACT FUNCTION  COMPONENT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function Intro() {
+  return <h1>Je suis sultan</h1>;
+}
+
+function Garage() {
+  return (
+    <>
+      <Intro />
+      <h3>Who lives in 02400 Château THierry</h3>
+
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Garage />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
